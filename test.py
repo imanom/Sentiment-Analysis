7b0cc3ab1d58c794421fa17138ff3a2855e29577
Monami <monamidg@gmail.com>
2020-05-01 16:33:00 +0530

    First commit

---
+++ test.py
@@ -0,0 +1,32 @@
+import nltk, flask
+import pickle as pickle
+from flask import request
+from features import clean
+nltk.download('wordnet')
+from sklearn.feature_extraction.text import TfidfVectorizer
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+
+def combine():
+	tokenizer = nltk.tokenize.TreebankWordTokenizer()
+	data= request.json['data'].strip()
+	tokens = (tokenizer.tokenize(data))
+	prediction = model.classify(dict([token, True] for token in tokens))
+	
+	return prediction
+
+
+
+@app.route('/sentiment', methods=['POST'])
+def home():
+	if not request.json or not 'data' in request.json:
+		abort(400)
+	return(combine())
+
+	
+if __name__ == '__main__':
+	modelfile = 'models/final_prediction.pickle'
+	model = pickle.load(open(modelfile, 'rb'))
+	app.run()
+	
